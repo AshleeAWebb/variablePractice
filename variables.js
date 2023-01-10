7043a033ddbc95195e6149d0a96b199e4397a88f
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-10 16:47:02 -0500

    Add console log and debug

--- variables.js
+++ variables.js
@@ -1,16 +1,36 @@
-var email = "email@gmail.com"
+var email = "email@gmail.com";
+console.log(email);
+
 var username = "name"
+console.log(username);
+
 var imageName = "image"
+console.log(imageName);
 
-var birthDate = 00/00/0000
-var date = 00/00/00000
-var bankBalance = 00000
 
-var userOnline = true
-var customerSeriviceAvaliable = false
-var onlineOrder = false
+var birthDate = 00;
+console.log(birthDate);
 
-var email = "ashlee.webb.18@gmail.com"
-var username = "ashwebb"
-var imageName = "webb selfie"
+var date = 00;
+console.log(date);
 
+var bankBalance = 00000;
+console.log(bankBalance);
+
+var userOnline = true;
+console.log(userOnline);
+
+var customerSeriviceAvaliable = false;
+console.log(customerSeriviceAvaliable);
+
+var onlineOrder = false;
+console.log(onlineOrder);
+
+var email = "ashlee.webb.18@gmail.com";
+console.log(email);
+
+var username = "ashwebb";
+console.log(username);
+
+var imageName = "webb selfie";
+console.log(imageName);
