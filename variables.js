4dd9a5edda25af332db6b20c75ae9149eeb0a735
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-10 16:40:09 -0500

    Fix missing quotation

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-var email = "email@gmail.com
+var email = "email@gmail.com"
 var username = "name"
 var imageName = "image"
 
@@ -9,3 +9,8 @@ var bankBalance = 00000
 var userOnline = true
 var customerSeriviceAvaliable = false
 var onlineOrder = false
+
+var email = "ashlee.webb.18@gmail.com"
+var username = "ashwebb"
+var imageName = "webb selfie"
+
